58fce304c2592dd9c2d21570b06e6a9b68f902a1
Derick Tseng <derickboss1@gmail.com>
2022-06-28 00:12:46 -0700

    roll back changes

--- lua/balance/init.lua
+++ lua/balance/init.lua
@@ -1,31 +1,15 @@
-local attr = require('balance.attr')
-local color = require('balance.colors')
-local utils = require('balance.utils')
-
--- the default options to load. Users can modify this by supplying their own options.
-local default_opt = {
-  attr = attr,
-  color = color
-}
-
 -- setup function. currently option doesn't work
-local function setup(user_opt)
+local function setup(options)
   -- initial boilerplate
-  vim.cmd("hi clear")
-
+  vim.cmd[[hi clear]]
   if vim.fn.exists('syntax on') then
-    vim.cmd('syntax reset')
+    vim.cmd[[syntax reset]]
   end
 
   vim.o.termguicolors = true
   vim.o.background = 'light'
   vim.g.colors_name = 'balance'
-
-  -- load user-supplied options, if they exist
-  local opt = default_opt
-  utils.merge_opts(opt, user_opt)
-
-  require('balance.theme').setup(opt)
+  require('balance.theme').setup(options)
 end
 
 return {setup = setup}
